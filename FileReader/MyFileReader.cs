0327040cd51ac2b966b388132fef12c44e167571
i-pereyaslovskiy <i.pereyaslovskiy@gmail.com>
2024-03-21 21:31:23 +0300

    Добавьте файлы проекта.

---
+++ FileReader/MyFileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otus_ParallelFilesReading.FileReader
+{
+    internal class MyFileReader
+    {
+        private int _spaceCount;
+        public int SpaceCount { get { return _spaceCount; } }
+
+        public Task CountAllSpacesAsync(string path)
+        {
+            return Task.Run(() =>
+            {
+                string readText = File.ReadAllText(path, Encoding.Default);
+                Interlocked.Add(ref _spaceCount, readText.ToCharArray().Count(i => i == ' '));
+            });
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using otus_ParallelFilesReading.FileReader;
+using System.Diagnostics;
+
+MyFileReader reader = new MyFileReader();
+Stopwatch stopwatch = new Stopwatch();
+
+var files = Directory.GetFiles("../../../FileStorage");
+
+stopwatch.Start();
+
+List<Task> tasks = new List<Task>();
+foreach (var filePath in files)
+  tasks.Add(Task.Run( async ()=> await reader.CountAllSpacesAsync(filePath)));
+
+await Task.WhenAll(tasks);
+
+stopwatch.Stop();
+
+Console.WriteLine($"Whitespaces count:{reader.SpaceCount}, Duration: {stopwatch.ElapsedMilliseconds} ms.");
+
+Console.ReadKey();
